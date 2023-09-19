497814971095db92512b3bed0e8c8f156edba2a2
rockfornik <nekmokshan@mail.ru>
2023-09-19 12:01:08 +0300

    #1 Начало

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.time.Duration;
+
+public class Main {
+
+    static WebDriver driver;
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chromedriver.driver", "D:\\chromedriver_win32\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.manage().window().maximize();
+
+        driver.get("https://mail.ru");
+        wait("(//*[text()='Войти'])[2]");
+    }
+
+    public static void wait(String xpath){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)));
+    }
+}

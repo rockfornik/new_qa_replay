3a4c4285f0366889485fa46cf389c3e40043e423
rockfornik <nekmokshan@mail.ru>
2023-09-27 16:39:34 +0300

    #6 Поменял-xpath-элементов-поиска-и-переделал-методы-поиска

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 import org.checkerframework.checker.units.qual.K;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -16,11 +17,8 @@ public class Main {
         System.setProperty("webdriver.chromedriver.driver", "D:\\chromedriver_win32\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("https://account.mail.ru/signup?from=navi&lang=ru_RU&siteid=169&app_id_mytracker=58519");
+        driver.get("https://mail.ru/");
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//div[@class=\"Select__control css-0\"])[1]")));
-        RegistrationPage registrationPage = new RegistrationPage(driver);
-        registrationPage.createCorrectMail("Ник", "Кипиш", "13", "Май", "1994", 'm', "nub14131516", "mail", "Q1w2e3r4t5y6", "nek@mail.ru");
 
     }
 }
